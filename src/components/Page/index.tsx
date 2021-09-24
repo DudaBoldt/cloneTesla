19613572bcacd65582cb65679f7ae55c5e6aa393
Maria Eduarda Boldt <edraude123@gmail.com>
2021-09-24 16:41:30 -0300

    Estrutura e scroll snap

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { ModelsWrapper, ModelSection } from '../Model'
+
+import DefaultOverlayContent from '../DefaultOverlayContent';
+
+import { Container } from './styles';
+
+
+const Page: React.FC = () => {
+return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          {[
+            'Modelo um',
+            'Modelo dois',
+            'Modelo tres',
+            'Modelo quatro',
+            'Modelo cinco',
+            'Modelo seis',
+            'Modelo sete'
+          ].map(modelName => (
+            <ModelSection 
+            key={modelName}
+            className="colored"
+            modelName={modelName}
+            overlayNode={
+              <DefaultOverlayContent 
+              label={modelName}
+              description="Modelo um, legal né"
+              />
+            }
+          />
+          ))}
+        </div>
+      </ModelsWrapper>
+    </Container>
+  );
+};
+
+export default Page;
